0667baa46fe593bebf9b7c2ca040233a78bcec2d
FEDERICO IAIR DRUCKER <49092806@ort>
2026-08-13 10:37:47 -0300

    hecho 100%

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const errorHandler = require("./middleware/errorHandler");
-
+const bcrypt = require('bcryptjs'); bcrypt.hash('admin123', 10).then(hash => console.log('Hash:', hash));
 const app = express();
 
 app.use(morgan("dev"));
--- src/data/db.js
+++ src/data/db.js
@@ -3,7 +3,7 @@ const users = [
     id: "1",
     name: "Admin",
     email: "admin@market.local",
-    password: "$2a$10$slYQmyNdGzin7olVN3p1OPST9/PgBkqx8d/LewY5YksVz57/Lixfm",
+    password: "$2a$10$GDjmH6p6KYmZYsmZXLX/5ulIRlRLN5BdslHXYfXpIF1voiBrb6.uq",
     role: "admin"
   }
 ];
